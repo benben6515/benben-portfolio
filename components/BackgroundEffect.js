731774746b1c575703649f631e5c85c115e383fc
Benben <z2266109@gmail.com>
2022-05-29 16:29:41 +0800

    fix: add more bubble!

--- components/BackgroundEffect.js
+++ components/BackgroundEffect.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BackgroundContainer, Bubbles, Bubble } from '../shared/backgroundStyles'
 
 const RandomBubbles = () => {
-  return Array(20).fill(null).map((e, i) => {
+  return Array(40).fill(null).map((e, i) => {
     const rate = Math.random() * 0.5 + 0.5
     const animationDuration = `${10 / rate}s`
     const animationDelay = `-${rate * 3}s`
